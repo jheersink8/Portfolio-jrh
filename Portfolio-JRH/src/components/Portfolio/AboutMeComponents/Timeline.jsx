d5e003274bdd5fffff847883745c9b4f53217f1a
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-11-02 21:51:36 -0600

    Timeline functionality is working!

--- Portfolio-JRH/src/components/Portfolio/AboutMeComponents/Timeline.jsx
+++ Portfolio-JRH/src/components/Portfolio/AboutMeComponents/Timeline.jsx
@@ -2,34 +2,16 @@ import timeline from '../../../assets/JSON/timeline.js';
 import '../../../assets/css/timeLine.css';
 import TimelineLayout from './TimelineComponents/TimelineLayout.jsx'
 
-
-
-
 export default function Timeline() {
-
-    const listItems = timeline.map(time =>
-        <li key={time.id}>
-            {time.year}: {time.event}
-        </li>
-    );
-
     return (
         <div>
-            {/* Timeline */}
-            <h3>My Timeline</h3>
-            <br></br>
-            <br></br>
+ 
 
 
-
-            <div className="container experience">
-                <div className="row">
-                    <div className="col-md-12">
-                    </div>
-
+        
+    
                     <TimelineLayout />
-                </div>
-            </div>
+            
 
         </div >
     );
--- Portfolio-JRH/src/components/Portfolio/AboutMeComponents/TimelineComponents/TimelineLayout.jsx
+++ Portfolio-JRH/src/components/Portfolio/AboutMeComponents/TimelineComponents/TimelineLayout.jsx
@@ -5,38 +5,41 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
-
+import { useState } from 'react';
 import timeline from '../../../../assets/JSON/timeline.js';
-const listItems = timeline.map(time =>  
-  <TimelineItem key={time.id}>
-    <TimelineSeparator>
-      <TimelineDot />
-      <TimelineConnector style={{ display: `${time.lineValue}` }} />
-    </TimelineSeparator>
-    <TimelineContent style={{}}>{time.year}</TimelineContent>
-  </TimelineItem>  
-);
 
 export default function TimelineLayout() {
+  const [index, setIndex] = useState(0);
+  function runYearSwitch(id) {
+    setIndex(id);
+  };
+
+  const listItems = timeline.map(time =>
+    <TimelineItem key={time.id}>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector style={{ display: `${time.lineValue}` }} />
+      </TimelineSeparator>
+      <TimelineContent><button onClick={() => runYearSwitch(time.id)}>{time.year}</button></TimelineContent>
+    </TimelineItem>
+  );
+
   return (
     <div>
-      <h3 style={{textAlign:'center'}}>My Timeline</h3>
-      <div className='container'>
-
-        <div className='row'>
-
-
-
-          <div className='col-3 border'>
-            <Timeline position="left">
-              {listItems}
-            </Timeline>
+      <h3 style={{ textAlign: 'center' }}>My Timeline</h3>
+      {/* Timeline */}
+      <div>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-3 border'>
+              <Timeline position="left">
+                {listItems}
+              </Timeline>
+            </div>
+            <div className='col-9 border d-flex align-items-center justify-content-center' >
+              <h4 >{timeline[index].event}</h4>
+            </div>
           </div>
-
-          <div className='col-9 border'>
-            <p>CONTENT AND STUFF</p>
-          </div>
-
         </div>
       </div>
     </div>
