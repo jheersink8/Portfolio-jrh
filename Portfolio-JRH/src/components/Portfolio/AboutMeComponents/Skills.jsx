636738f523d7a37f8428cdb2a9d5579411af6a6f
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-12-01 20:31:00 -0700

    Convert svg to jsx

--- Portfolio-JRH/src/components/Portfolio/AboutMeComponents/Skills.jsx
+++ Portfolio-JRH/src/components/Portfolio/AboutMeComponents/Skills.jsx
@@ -1,5 +1,22 @@
 import skills from '../../../assets/JSON/skills';
 
+import Api from '../../../assets/svg/skills/Api';
+import Bootstrap from '../../../assets/svg/skills/Bootstrap';
+// import Css from '../../../assets/svg/skills/Css';
+// import Express from '../../../assets/svg/skills/Express';
+import Github from '../../../assets/svg/skills/Github';
+import Graphql from '../../../assets/svg/skills/Graphql';
+import Heroku from '../../../assets/svg/skills/Heroku';
+// import Html from '../../../assets/svg/skills/Html';
+import Javascript from '../../../assets/svg/skills/Javascript';
+// import Jquery from '../../../assets/svg/skills/Jquery';
+import Mongodb from '../../../assets/svg/skills/Mongodb';
+import Node from '../../../assets/svg/skills/Node';
+import Npm from '../../../assets/svg/skills/Npm';
+import Postgresql from '../../../assets/svg/skills/Postgresql';
+import Reactsvg from '../../../assets/svg/skills/Reactsvg';
+// import Sql from '../../../assets/svg/skills/Sql';
+
 
 export default function Skills() {
     const listItems = skills.map(skill =>
@@ -11,6 +28,12 @@ export default function Skills() {
         <div>
             {/* Skills */}
             <h3>My Web Development Toolbelt</h3>
+            <div className='border' style={{width: '50%'}}>
+                <Api />
+            </div>
+
+
+            
             <ul>
                 {listItems}
 
