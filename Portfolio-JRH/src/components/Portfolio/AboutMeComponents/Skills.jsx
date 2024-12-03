c123561069f904a62359a9c375fe72930259ab30
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-12-03 11:21:54 -0700

    Cleaned up SVG files.

--- Portfolio-JRH/src/components/Portfolio/AboutMeComponents/Skills.jsx
+++ Portfolio-JRH/src/components/Portfolio/AboutMeComponents/Skills.jsx
@@ -1,24 +1,28 @@
 import skills from '../../../assets/JSON/skills';
 
-import Api from '../../../assets/svg/skills/Api';
-import Bootstrap from '../../../assets/svg/skills/Bootstrap';
-// import Css from '../../../assets/svg/skills/Css';
-// import Express from '../../../assets/svg/skills/Express';
-import Github from '../../../assets/svg/skills/Github';
-import Graphql from '../../../assets/svg/skills/Graphql';
-import Heroku from '../../../assets/svg/skills/Heroku';
-// import Html from '../../../assets/svg/skills/Html';
-import Javascript from '../../../assets/svg/skills/Javascript';
-// import Jquery from '../../../assets/svg/skills/Jquery';
-import Mongodb from '../../../assets/svg/skills/Mongodb';
-import Node from '../../../assets/svg/skills/Node';
-import Npm from '../../../assets/svg/skills/Npm';
-import Postgresql from '../../../assets/svg/skills/Postgresql';
-import Reactsvg from '../../../assets/svg/skills/Reactsvg';
-// import Sql from '../../../assets/svg/skills/Sql';
+import ApiSVG from '../../../assets/svg/skills/ApiSVG';
+import BootstrapSVG from '../../../assets/svg/skills/BootstrapSVG';
+import CssSVG from '../../../assets/svg/skills/CssSVG';
+import ExpressSVG from '../../../assets/svg/skills/ExpressSVG';
+import GithubSVG from '../../../assets/svg/skills/GithubSVG';
+import GraphqlSVG from '../../../assets/svg/skills/GraphqlSVG';
+import HerokuSVG from '../../../assets/svg/skills/HerokuSVG';
+import HtmlSVG from '../../../assets/svg/skills/HtmlSVG';
+import JavascriptSVG from '../../../assets/svg/skills/JavascriptSVG';
+import JquerySVG from '../../../assets/svg/skills/JquerySVG';
+import MongodbSVG from '../../../assets/svg/skills/MongodbSVG';
+import NodeSVG from '../../../assets/svg/skills/NodeSVG';
+import NpmSVG from '../../../assets/svg/skills/NpmSVG';
+import PostgresqlSVG from '../../../assets/svg/skills/PostgresqlSVG';
+import ReactSVG from '../../../assets/svg/skills/ReactSVG';
+import SqlSVG from '../../../assets/svg/skills/SqlSVG';
 
 
 export default function Skills() {
+    let svgH =120;
+    let svgW =120;
+    let color = '#ffffff';
+    
     const listItems = skills.map(skill =>
         <li key={skill.id}>
             {skill.mastery}
@@ -28,8 +32,23 @@ export default function Skills() {
         <div>
             {/* Skills */}
             <h3>My Web Development Toolbelt</h3>
-            <div className='border' style={{width: '50%'}}>
-                <Api />
+            <div className='border'>
+                <ApiSVG width={svgW} height={svgH} color={color}/>
+                <BootstrapSVG width={svgW} height={svgH} color={color}/>
+                <CssSVG width={svgW} height={svgH} color={color}/>
+                <ExpressSVG width={svgW} height={svgH} color={color}/>
+                <GithubSVG width={svgW} height={svgH} color={color}/>
+                <GraphqlSVG width={svgW} height={svgH} color={color}/>
+                <HerokuSVG width={svgW} height={svgH} color={color}/>
+                <HtmlSVG width={svgW} height={svgH} color={color}/>
+                <JavascriptSVG width={svgW} height={svgH} color={color}/>
+                <JquerySVG width={svgW} height={svgH} color={color}/>
+                <MongodbSVG width={svgW} height={svgH} color={color}/>
+                <NodeSVG width={svgW} height={svgH} color={color}/>
+                <NpmSVG width={svgW} height={svgH} color={color}/>
+                <PostgresqlSVG width={svgW} height={svgH} color={color}/>
+                <ReactSVG width={svgW} height={svgH} color={color}/>
+                <SqlSVG width={svgW} height={svgH} color={color}/>
             </div>
 
 
